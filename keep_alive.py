4f4fb44ed07507d90c65b2df45ea19be27a662be
melker23 <melker.axell@gmail.com>
2024-01-22 16:18:31 +0100

    style: more blank lines

--- keep_alive.py
+++ keep_alive.py
@@ -3,12 +3,15 @@ from threading import Thread
 
 app = Flask('')
 
+
 @app.route('/')
 def home():
     return "I'm alive!"
 
+
 def run():
     app.run(host='0.0.0.0', port=8080)
+
 
 def keep_alive():
     t = Thread(target=run)
